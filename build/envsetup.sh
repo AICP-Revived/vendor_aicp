LineageOS/android_vendor_lineage

Fix quoting in argument forward of `repopick`
`repopick --query 'topic:Foo project:Bar'` currently fails as the
argument to `--query` gets split making it appear as a query and change
number argument to `repopick.py`
So forward the correct quoting.

Change-Id: I020d38216303051cbeb48cfac1c9b2f8738e02f9

build/envsetup.sh
@@ -920,7 +920,7 @@ alias cmkap='dopush cmka'
 
 function repopick() {
     T=$(gettop)
-    $T/vendor/lineage/build/tools/repopick.py $@
+    $T/vendor/lineage/build/tools/repopick.py "$@"
 }
 
 function fixup_common_out_dir() {

AICP-Revived/vendor_aicp

build/envsetup.sh
@@ -970,7 +970,7 @@ alias cmkap='dopush cmka'
 
 function repopick() {
     T=$(gettop)
-    $T/vendor/aicp/build/tools/repopick.py $@
+    $T/vendor/aicp/build/tools/repopick.py "$@"
 }
 
 function sort-blobs-list() {